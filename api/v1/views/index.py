c3e1c0cf215a525e4dcca73d11fec3cd2d1bea53
PabloOsorix <3387@holbertonschool.com>
2022-05-29 22:30:36 -0500

    :heavy_plus_sign: add route stats lines[16:28]

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,11 +2,26 @@
 """Module with a Blueprint object that have one
 route with /status lie an end point"""
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route("/status", strict_slashes=False)
 def status():
     """Return JSON with the status of the API"""
-    obj = {"status": "OK"}
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """Route that return the number of each object
+    by type"""
+    number_of_objects = {
+        "amenities": len(storage.all("Amenity")),
+        "cities": len(storage.all("City")),
+        "places": len(storage.all("Place")),
+        "reviews": len(storage.all("Review")),
+        "states": len(storage.all("State")),
+        "users": len(storage.all("User"))
+        }
+    return jsonify(number_of_objects)
