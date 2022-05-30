dbaded08d5ab20efd99eec0b501048d284404602
PabloOsorix <3387@holbertonschool.com>
2022-05-29 22:58:35 -0500

    :heavy_plus_sign: add make_response line 19

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,8 +3,7 @@
 it contain one route.
 """
 from os import getenv
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views.index import app_views
 
@@ -13,17 +12,17 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
-def close(execute):
-    """Instance that close conection with engine"""
-    storage.close()
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     """Handler to the error 404 when a url(uri) is not
     found"""
-    return jsonify({"error": "Not found"})
+    return make_response(jsonify({"error": "Not found"}), 400)
+
+
+@app.teardown_appcontext
+def close(execute):
+    """Instance that close conection with engine"""
+    storage.close()
 
 
 if __name__ == "__main__":
