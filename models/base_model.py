b6577ddd2770e7e99e26d3fbddbcf2644b6b76b7
pipeleon <oflg94@gmail.com>
2022-05-31 15:56:37 -0500

    update models for secure password

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
+import hashlib
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -31,6 +32,9 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
+                    if key == "password":
+                        m = hashlib.md5(value.encode())
+                        value = m.hexdigest()
                     setattr(self, key, value)
             if kwargs.get("created_at", None) and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
@@ -68,6 +72,8 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if "password" in new_dict and models.storage_t == "db":
+            del new_dict["password"]
         return new_dict
 
     def delete(self):
--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,9 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+    
+    @password.setter
+    def password(self, value):
+        m = hashlib.md5(value.encode())
+        value = m.hexdigest()
+        self.password = value
