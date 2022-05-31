b37cb685dcd564cdb4dd22959c0408308c5f7716
PabloOsorix <3387@holbertonschool.com>
2022-05-30 20:55:08 -0500

    :pencil2: fix pep8

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -12,7 +12,7 @@ from flask import jsonify, make_response, request, abort
 
 @app_views.route("/users", methods=['GET'], strict_slashes=False)
 @app_views.route("/users/<user_id>", methods=['GET'], strict_slashes=False)
-def user_or_user(user_id=None):
+def users_or_user(user_id=None):
     """Routes to return a list of Users or
     a specific User with a given id."""
     if user_id is None:
@@ -29,12 +29,13 @@ def user_or_user(user_id=None):
         return jsonify(user.to_dict())
 
 
-@app_views.route("/users/<user_id>", methods=['DELETE'], strict_slashes=False)
+@app_views.route("/users/<user_id>", methods=["DELETE"], strict_slashes=False)
 def delete_user(user_id=None):
     """Route that delete an user with a given
     id.
     (Object Class User) del_user = user to
                         delete."""
+    del_user = {}
     del_user = storage.get(User, user_id)
     if not del_user:
         abort(404)
@@ -53,9 +54,9 @@ def new_user():
     inf_user = request.get_json
     if not inf_user:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
-    if not inf_user["email"]:
+    if "email" not in inf_user:
         return make_response(jsonify({"error": "Missing email"}), 400)
-    if not inf_user["password"]:
+    if "password" not in inf_user:
         return make_response(jsonify({"error": "Missing password"}), 400)
     new_user = User(**inf_user)
     storage.new(new_user)
