d2ee516c421ae334d6290de7ec38481c375a4469
pipeleon <oflg94@gmail.com>
2022-05-31 14:20:15 -0500

    update Places-Amenity API for db mode

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -9,6 +9,7 @@ from models.amenity import Amenity
 from models.place import Place
 from api.v1.views import app_views
 from flask import jsonify, make_response, request, abort
+from os import getenv
 
 
 @app_views.route("/places/<place_id>/amenities", methods=['GET'], strict_slashes=False)
@@ -19,12 +20,59 @@ def amenities_by_place(place_id=None):
     if not place:
         abort(404)
     
-    test = place.amenities
+    objs = place.amenities
 
     list_amen = []
-    for obj in test:
+    for obj in objs:
         list_amen.append(obj.to_dict())
     
     return jsonify(list_amen)
 
 
+@app_views.route("/places/<place_id>/amenities/<amenity_id>", methods=['DELETE'], strict_slashes=False)
+def del_amenity(place_id=None, amenity_id=None):
+    """Deletes an Amenity from a selected Place
+     with a given id."""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenenity = storage.get(Amenity, amenity_id)
+    if not amenenity:
+        abort(404)
+
+    objs = place.amenities
+
+    for obj in objs:
+        if obj.id == amenity_id:
+            place.amenities.remove(obj)
+            flag = 1
+            break
+        else:
+            flag = 0
+
+    if flag == 1:
+        return make_response(jsonify({}), 200)
+    else:
+        abort(404)
+
+
+@app_views.route("/places/<place_id>/amenities/<amenity_id>", methods=['POST'], strict_slashes=False)
+def link_amenity(place_id=None, amenity_id=None):
+    """Links an Amenity from a selected Place
+     with a given id."""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenenity = storage.get(Amenity, amenity_id)
+    if not amenenity:
+        abort(404)
+
+    objs = place.amenities
+
+    for obj in objs:
+        if obj.id == amenity_id:
+            return make_response(jsonify(obj.to_dict()), 200)
+
+    place.amenities.append(amenenity)
+
+    return make_response(jsonify(amenenity.to_dict()), 201)
