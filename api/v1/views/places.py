4f1142233689f6567ecfdd2a40f7b397d236c767
pipeleon <oflg94@gmail.com>
2022-06-01 13:43:25 -0500

    fixing case for place without atributte amenities

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -165,7 +165,7 @@ def places_search():
             count = 0
             for amenity_id in list_amenities:
                 amenity = storage.get(Amenity, amenity_id)
-                if "amenities" in place.__dict__:
+                if hasattr(place, "amenities"):
                     if amenity in place.amenities:
                         count += 1
             if count == len(list_amenities):
